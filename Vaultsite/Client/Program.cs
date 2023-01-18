d2ec33dc8a126930087e3d9dadfba812bebc41a7
renep <renep@RENEPC>
2023-01-18 16:42:57 +0100

    Added Vaultcontroller Removed WeatherForecastController

--- Vaultsite/Client/Program.cs
+++ Vaultsite/Client/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.Toast;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using VaultSite;
@@ -11,7 +12,7 @@ namespace VaultSite
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
-
+            builder.Services.AddBlazoredToast();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
